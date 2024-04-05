9a74acef8ac0d9bd4aa35145fcf90855251bd9d6
Jere Lehtinen <jere@Jeres-Laptop.local>
2024-04-05 14:54:43 +0300

    separate timeline and timelineitem

--- src/app/timeline-item/timeline-item.component.ts
+++ src/app/timeline-item/timeline-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TimelineItem } from '../timelineItem';
 
 @Component({
   selector: 'app-timeline-item',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './timeline-item.component.css'
 })
 export class TimelineItemComponent {
-
+  @Input() timelineItem?: TimelineItem;
 }
