64c023ee4cd404c7ffff3b77f196ba1ab73f3144
Jere Lehtinen <jere@Jeres-Laptop.local>
2024-09-07 21:57:45 +0300

    wip
    	# Please enter the commit message for your changes. Lines starting
    	# Committer: Jere Lehtinen <jere@Jeres-Laptop.local>
    		#

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { PORTOLIOITEMS } from '../../data/list-portofolioItems';
+
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
 export class PortfolioComponent {
-  details = 'portfolio'
+  portfolioItems = PORTOLIOITEMS;
 }
--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NgFor } from '@angular/common';
 
 import { EDUCATIONITEMS } from '../../data/list-educationItems';
 import { WORKITEMS } from '../../data/list-workItems';
