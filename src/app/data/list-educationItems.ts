98dbd78bdeb38c8f1115da1a06fc224bc246938f
Jere Lehtinen <jere@Jeres-Laptop.local>
2024-09-09 19:02:38 +0300

    modify links and texts

--- src/app/data/list-educationItems.ts
+++ src/app/data/list-educationItems.ts
@@ -6,13 +6,12 @@ export const EDUCATIONITEMS: TimelineItem[] =
             year: '2021',
             title: "Master's Degree in Computer Science | University of Jyväskylä",
             duration: 'today',
-            details: "I am working on my master's thesis, which focuses on cost-effective " +
-             "monitoring for Cloud-based SaaS software."
+            details: "I'm majoring in Software Engineering with a Grade Point Average of 4.05/5. I am currently finishing my Master's thesis on cost-effective monitoring of cloud-based SaaS systems."
         },
         {
             year: '2015',
             title: "Bachelor's Degree in Electrical engineering | Jamk University of Applied Sciences",
             duration: '4 years',
-            details: "For my bachelor's thesis, I developed a learning environment for transformer protection."
+            details: "I graduated with a Grade Point Average of 3.49/5. For my bachelor's thesis, I developed a learning environment for transformer protection."
         },
     ]
